7a5546a470f1bc07185639fd1c01f2eb0fd4652d
Jordan Developper <77929953+pexilis@users.noreply.github.com>
2021-04-27 14:26:05 +0200

    update feature

--- cart.js
+++ cart.js
@@ -1,29 +1,106 @@
+const LocalStorageAPI = (() => {
+    let self = {};
+
+    self.getArray = name => JSON.parse(localStorage.getItem(name));
+    self.isInitialized = name => localStorage.getItem(name) !== null;
+    self.clearArray = item => localStorage.setItem(item, "[]");
+    
+    self.getById = (name, id) => {
+        const arrArticle = self.getArray(name);
+        const item = arrArticle.find(article => article.id === id);
+        return item;
+    };
+
+    self.addItem = (name, item) => {
+        const arrArticle = self.getArray(name);
+        arrArticle.push(item);
+        localStorage.setItem(name, JSON.stringify(arrArticle));
+    }
+
+    self.setById = (name, id, newItem) => {
+        let arrArticle = self.getArray(name);
+        
+        arrArticle = arrArticle.filter(item => item.id === id)
+                               .map(modify => modify = newItem);
+        localStorage.setItem(name, JSON.stringify(arrArticle));
+    }
+
+    return self;
+})();
+
+
 const Cart = (() => {
     let self = {};
 
     const cartName = "cart-storage";
 
-    const isInitialized = name => getItem(name) !== null;
-
-    const getItem = item => localStorage.getItem(item);
-
     self.listArticles = async() => {
         errorInit(cartName);
-        let result = JSON.parse(getItem(cartName));
+
+        let result = LocalStorageAPI.getArray(cartName);
         return Promise.resolve(result);
     }
 
     const errorInit = name => {
-        if (!isInitialized(name)) throw {error:"INITIALIZATION_ERROR"};
+        const isInit =  !LocalStorageAPI.isInitialized(name);
+        if (isInit) throw {error:"INITIALIZATION_ERROR"};
     }
 
     const errorEmpty = name => {
-        if (getItem(name) === "[]") throw {error:"EMPTY_ERROR"};
+        const isEmpty = LocalStorageAPI.getArray(name).length === 0;
+        if (isEmpty) throw {error:"EMPTY_ERROR"};
     }
 
-    self.clearCart = async() => {
+    const errorFormat = article => {
+        const regexUUID = /^[0-9a-z]{24}$/;
+        const regexQUANTITY = /^[0-9]{1,2}$/;
+        const regexPRICE = /^[0-9]{1,}00$/;
+        const regexURL = /^http:\/\/localhost:3000\/images\/vcam_[0-9]{1,}\.jpg$/;
+
+        if (!regexUUID.test(article.id) || !regexQUANTITY.test(article.quantity) || !regexPRICE.test(article.price) || !regexURL.test(article.imageUrl)) 
+            throw {error:"FORMAT_ERROR"};
+    }
+
+    const calculateQuantity = (old_quantity, new_quantity) => {
+        return old_quantity + new_quantity;
+    }
+
+    self.clearCart = () => {
         errorInit(cartName);
         errorEmpty(cartName);
+        LocalStorageAPI.clearArray(cartName);
+    }
+
+    const updateArticleWithNewQuantity = (old,article) => {
+        const new_quantity = calculateQuantity
+                            (
+                                Number.parseInt(old.quantity), 
+                                Number.parseInt(article.quantity)
+                            )
+                           .toString();
+
+        article.quantity = new_quantity;
+
+        try{
+            errorFormat(article);
+        }catch(e){
+            throw {error:"ADD_ERROR"};
+        }
+
+        LocalStorageAPI.setById(cartName, article.id, article);
+    }
+
+    self.addArticle = (article) => {
+        errorInit(cartName);
+        errorFormat(article);
+
+        const old = LocalStorageAPI.getById(cartName, article.id);
+
+        if (old){
+            updateArticleWithNewQuantity(old, article);
+        }else{
+            LocalStorageAPI.addItem(cartName, article);
+        }
     }
 
     return self;
--- test/cart.test.js
+++ test/cart.test.js
@@ -9,20 +9,255 @@ describe("See all articles", () => {
     it("should return every articles when all is ok", () => {
         expect.assertions(1);
         localStorage.setItem("cart-storage", "[]");
-        return Cart.listArticles().then(data => expect(data).toEqual([]));
+        return Cart.listArticles().then(data => expect(typeof data === "object").toBe(true));
     });
 });
 
 describe("Empty the cart", () => {
     it("should throw an error when the cart is empty", () => {
-        expect.assertions(1);
-        localStorage.setItem("cart-storage", "[]");
-        return Cart.clearCart().catch(e => expect(e).toEqual({error:"EMPTY_ERROR"}));
+        let thrownError;
+        try{
+            localStorage.setItem("cart-storage", "[]");
+            Cart.clearCart();
+        }catch(error){
+            thrownError = error;
+        }
+
+        expect(thrownError).toEqual({error:"EMPTY_ERROR"});
     });
 
     it("should throw an error when the cart isn't initialized", () => {
-        localStorage.removeItem("cart-storage");
-        expect.assertions(1);
-        return Cart.clearCart().catch(e => expect(e).toEqual({error:"INITIALIZATION_ERROR"}));
+        let thrownError;
+        try{
+            localStorage.removeItem("cart-storage");
+            Cart.clearCart();
+        }catch(error){
+            thrownError = error;
+        }
+
+        expect(thrownError).toEqual({error:"INITIALIZATION_ERROR"});
     });
-})
+
+    it("should empty the basket when everything is ok", () => {
+            const dummyCart = [
+                {
+                    name:"teddy-red",
+                    price:"1023$"
+                },
+                {
+                    name:"teddy-red",
+                    price:"1023$"
+                }
+            ];
+
+            localStorage.setItem("cart-storage", JSON.stringify(dummyCart));
+            Cart.clearCart();
+            const array = JSON.parse(localStorage.getItem("cart-storage"));
+            expect(array.length === 0).toBe(true);
+    });
+});
+
+describe("Add article to cart", () => {
+    it("should throw an error when the cart isn't initizalized", () => {
+        let thrownError;
+        try{
+            localStorage.removeItem("cart-storage");
+            Cart.addArticle();
+        }catch(error){
+            thrownError = error;
+        }
+
+        expect(thrownError).toEqual({error:"INITIALIZATION_ERROR"});
+    });
+
+    it("should throw an error when the uuid isn't proper format", () => {
+        let thrownError;
+        try{
+            let dummyArticle = {
+                name:"foo",
+                description:"my description",
+                id:"5be1ed3f1c9d440000", 
+                quantity:"5",
+                price:"4500",
+                imageUrl:"http://localhost:3000/images/vcam_1.jpg"
+            }
+
+            localStorage.setItem("cart-storage", "[]");
+            Cart.addArticle(dummyArticle);
+        }catch(error){
+            thrownError = error;
+        }
+
+        expect(thrownError).toEqual({error:"FORMAT_ERROR"});
+    });
+
+    it("should throw an error when the quantity is negative", () => {
+        let thrownError;
+        try{
+            let dummyArticle = {
+                name:"foo",
+                description:"my description",
+                id:"5be1ed3f1c9d44000030b061", 
+                quantity:"-8",
+                price:"4500",
+                imageUrl:"http://localhost:3000/images/vcam_1.jpg"
+            }
+            
+            localStorage.setItem("cart-storage", "[]");
+            Cart.addArticle(dummyArticle);
+        }catch(error){
+            thrownError = error;
+        }
+
+        expect(thrownError).toEqual({error:"FORMAT_ERROR"});
+    });
+
+    it("should throw an error when the quantity is higher than 99", () => {
+        let thrownError;
+        try{
+            let dummyArticle = {
+                name:"foo",
+                description:"my description",
+                id:"5be1ed3f1c9d44000030b061", 
+                quantity:"100",
+                price:"4500",
+                imageUrl:"http://localhost:3000/images/vcam_1.jpg"
+            }
+            
+            localStorage.setItem("cart-storage", "[]");
+            Cart.addArticle(dummyArticle);
+        }catch(error){
+            thrownError = error;
+        }
+
+        expect(thrownError).toEqual({error:"FORMAT_ERROR"});
+    });
+
+    it("should throw an error when the price isn't proper format", () => {
+        let thrownError;
+        try{
+            let dummyArticle = {
+                name:"foo",
+                description:"my description",
+                id:"5be1ed3f1c9d44000030b061", 
+                quantity:"5",
+                price:"4555",
+                imageUrl:"http://localhost:3000/images/vcam_1.jpg"
+            }
+            
+            localStorage.setItem("cart-storage", "[]");
+            Cart.addArticle(dummyArticle);
+        }catch(error){
+            thrownError = error;
+        }
+
+        expect(thrownError).toEqual({error:"FORMAT_ERROR"});
+    });
+
+    it("should throw an error when the imageURL isn't proper format", () => {
+        let thrownError;
+        try{
+            let dummyArticle = {
+                name:"foo",
+                description:"my description",
+                id:"5be1ed3f1c9d44000030b061", 
+                quantity:"5",
+                price:"455500",
+                imageUrl:"http://localhost:3000/images/vam_1.jpg"
+            }
+            
+            localStorage.setItem("cart-storage", "[]");
+            Cart.addArticle(dummyArticle);
+        }catch(error){
+            thrownError = error;
+        }
+
+        expect(thrownError).toEqual({error:"FORMAT_ERROR"});
+    });
+
+    it("should throw an error when the article isn't proper format", () => {
+        let thrownError;
+        try{
+            let dummyArticle = {
+                toto:"5be1ed3f1c9d44000030b061", 
+                tutu:"100"
+            }
+            
+            localStorage.setItem("cart-storage", "[]");
+            Cart.addArticle(dummyArticle);
+        }catch(error){
+            thrownError = error;
+        }
+
+        expect(thrownError).toEqual({error:"FORMAT_ERROR"});
+    });
+
+    it("should add article when article doesn't exist", () => {
+        localStorage.setItem("cart-storage", "[]");
+        const id = "5be1ed3f1c9d44000030b061";
+        const quantity = "50";
+
+        let dummyArticle = {
+                name:"foo",
+                description:"my description",
+                id:"5be1ed3f1c9d44000030b061", 
+                quantity:"5",
+                price:"455500",
+                imageUrl:"http://localhost:3000/images/vcam_1.jpg"
+        };
+
+        Cart.addArticle(dummyArticle);
+
+        const arrArticle = JSON.parse(localStorage.getItem("cart-storage"));
+        const article = arrArticle.find(article => article.id === id);
+
+        expect(article).toEqual(dummyArticle);
+    });
+
+    it("update the cart with the new quantity added if the item already exists", () => {
+        localStorage.setItem("cart-storage", "[]");
+        const id = "5be1ed3f1c9d44000030b061";
+        
+        let dummyArticle = {
+                name:"foo",
+                description:"my description",
+                id:"5be1ed3f1c9d44000030b061", 
+                quantity:"10",
+                price:"455500",
+                imageUrl:"http://localhost:3000/images/vcam_123.jpg"
+        };
+
+        Cart.addArticle(dummyArticle);
+        Cart.addArticle(dummyArticle);
+
+        const arrArticle = JSON.parse(localStorage.getItem("cart-storage"));
+        const article = arrArticle.find(article => article.id === id);
+
+        expect(article.quantity).toEqual("20");
+    });
+
+    it("Return an error when the sum is not in the right format", () => {
+        let thrownError;
+
+        try{
+            localStorage.setItem("cart-storage", "[]");
+            const id = "5be1ed3f1c9d44000030b061";
+        
+            let dummyArticle = {
+                name:"foo",
+                description:"my description",
+                id:"5be1ed3f1c9d44000030b061", 
+                quantity:"90",
+                price:"455500",
+                imageUrl:"http://localhost:3000/images/vcam_1.jpg"
+            };
+
+            Cart.addArticle(dummyArticle);
+            Cart.addArticle(dummyArticle);
+        }catch(e){
+            thrownError = e;
+        }
+        
+        expect(thrownError).toEqual({error:"ADD_ERROR"});
+    });
+});
