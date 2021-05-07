0690be8ef23858af381600f5ab3242c81fe9935c
Jordan Developper <77929953+pexilis@users.noreply.github.com>
2021-05-07 13:54:00 +0200

    Refactoring: add DOM and JSON utils

---
+++ frontend/js/utils/JSONManip.js
@@ -0,0 +1,44 @@
+const JSONManip = (() => {
+    let self = {};
+
+    self.stringifyJSON = (json) => {
+        const validJS = (json !== null && typeof json === "object");
+        if (!validJS)
+            return false; 
+
+       
+        Object.entries(json).map(([key, value]) => {
+            if (typeof value === "number")
+                json[key] = value.toString();  
+            self.stringifyJSON(value)
+        });
+    }
+
+    self.genericJSON = async(response) => {
+        let json;
+
+        try{
+            json = await response.json();
+        }catch(e){
+            throw {error:"ERROR_JSON"}
+        }
+
+        self.stringifyJSON(json);
+        return json;
+    }
+        
+    self.formToJSON = (form, keys) => {
+        const formData = new FormData(form);
+        const jsonDict = {};
+        keys.map(key => {
+            const value = formData.get(key);
+            jsonDict[key] = value;
+        });
+
+        return jsonDict;
+    }
+    
+    return self;
+})();
+
+export default JSONManip;
